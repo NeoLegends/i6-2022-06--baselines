c1d5b236909a507aa31153b4feaa4e0deadbae9f
Moritz Gunz <mgunz@i6.informatik.rwth-aachen.de>
2022-07-31 17:58:17 +0200

    use nick's python

--- settings.py
+++ settings.py
@@ -29,7 +29,8 @@ def engine():
 
 MAIL_ADDRESS = getpass.getuser()
 
-SIS_COMMAND = ['/work/tools/asr/python/3.8.0_tf_1.15-generic+cuda10.1/bin/python3.8', sys.argv[0]]
+# SIS_COMMAND = ['/work/tools/asr/python/3.8.0_tf_1.15-generic+cuda10.1/bin/python3.8', sys.argv[0]]
+SIS_COMMAND = ['/work/asr3/rossenbach/schuemann/new_simple_tts_env/bin/python', sys.argv[0]]
 
 #/work/asr4/rossenbach/env/python38_sisyphus/bin/python
 
@@ -59,8 +60,9 @@ SCTK_PATH = '/u/beck/programs/sctk-2.4.0/bin/'
 #RETURNN_PYTHON_EXE='/work/tools/asr/python/3.8.0_tf_2.3-v1-generic+cuda10.1/bin/python'
 #RETURNN_PYTHON_HOME='/work/tools/asr/python/3.8.0_tf_2.3.4-generic+cuda10.1+mkl'
 #RETURNN_PYTHON_EXE='/work/tools/asr/python/3.8.0_tf_2.3.4-generic+cuda10.1+mkl/bin/python3'
-RETURNN_PYTHON_HOME='/work/tools/asr/python/3.8.0_tf_1.15-generic+cuda10.1'
-RETURNN_PYTHON_EXE='/work/tools/asr/python/3.8.0_tf_1.15-generic+cuda10.1/bin/python3.8'
+#RETURNN_PYTHON_HOME='/work/tools/asr/python/3.8.0_tf_1.15-generic+cuda10.1'
+#RETURNN_PYTHON_EXE='/work/tools/asr/python/3.8.0_tf_1.15-generic+cuda10.1/bin/python3.8'
+RETURNN_PYTHON_EXE='/u/rossenbach/bin/returnn_tf2.3_launcher.sh'
 #RETURNN_PYTHON_HOME = '/u/beck/programs/python/2.7.10/'
 #RETURNN_PYTHON_EXE  = '/u/beck/programs/python/2.7.10/bin/python2.7'
 #RETURNN_ROOT        = '/u/raissi/setups/swb/crnn/'
