626a7cdb9af3001d49ef2b3df18eed3f6928b413
Moritz Gunz <moritz.gunz@gmail.com>
2022-08-09 11:09:40 +0200

    also diphone training

--- config/config.py
+++ config/config.py
@@ -1,2 +1,2 @@
-N_PHONES = [1, 3]  # , 2]
+N_PHONES = [1, 2, 3]
 RAISSI_ALIGNMENT = "/work/asr4/raissi/setups/librispeech/960-ls/work/i6_core/mm/alignment/AlignmentJob.hK21a0UU4iiJ/output/alignment.cache.bundle"
