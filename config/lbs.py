28fbe89bfc17ce3b1aedef0c0ce4a7543b773736
Moritz Gunz <moritz.gunz@gmail.com>
2022-08-10 11:42:29 +0200

    import data from personal helper

--- config/lbs.py
+++ config/lbs.py
@@ -1,11 +1,11 @@
 import copy
 
 import i6_experiments.common.datasets.librispeech as lbs_dataset
-import i6_experiments.users.raissi.experiments.librispeech.data_preparation.other_960h.pipeline_base_args as data_setups
+import i6_private.users.gunz.setups.ls.pipeline_rasr_args as data_setups
 
 
 def get_data_4gram_lm():
-    return data_setups.get_data_inputs()
+    return data_setups.get_data_inputs(add_unknown_phoneme_and_mapping=True)
 
 
 def get_data_lstm_lm():
@@ -15,7 +15,7 @@ def get_data_lstm_lm():
         "scale": 10,
     }
 
-    train, dev, test = data_setups.get_data_inputs()
+    train, dev, test = data_setups.get_data_inputs(add_unknown_phoneme_and_mapping=True)
     for corpus in list(dev.values()) + list(test.values()):
         corpus.lm = copy.deepcopy(lm_lstm)
     return train, dev, test
