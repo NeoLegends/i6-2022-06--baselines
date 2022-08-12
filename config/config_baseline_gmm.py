4dbe468d56c65e8d0c7ca00232844ad98b834d7f
Moritz Gunz <moritz.gunz@gmail.com>
2022-08-12 12:46:37 +0200

    use 12k leaves again

--- config/config_baseline_gmm.py
+++ config/config_baseline_gmm.py
@@ -38,7 +38,7 @@ def _run_gmm(
     )
 
     mono_args = gmm_setups.get_monophone_args(allow_zero_weights=True)
-    cart_di_args = gmm_setups.get_cart_args(name="di", phones=2, max_leaves=5043)
+    cart_di_args = gmm_setups.get_cart_args(name="di", phones=2)
     cart_tri_args = gmm_setups.get_cart_args()
     tri_args = gmm_setups.get_triphone_args()
     final_output_args = gmm_setups.get_final_output()
