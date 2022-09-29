27e933509feaf3997623c5a068f1652b3bd3033f
Moritz Gunz <moritz.gunz@gmail.com>
2022-09-29 13:08:08 +0200

    allow running hy and fh separately

--- config/__init__.py
+++ config/__init__.py
@@ -2,26 +2,26 @@ from sisyphus import gs, tk, Path
 
 import i6_core.tools as tools
 
-from i6_private.users.gunz.experiments.config_2022_07_baselines import (
-    config_10_baseline_gmm_mono as mono,
-    config_11_baseline_gmm_di as di,
-    config_12_baseline_gmm_tri as tri,
-    config_20_baseline_hybrid as hybrid,
-    config_22_baseline_blstm as blyadstm,
-    config_30_baseline_fh_mono as fh_mono,
-    config_31_baseline_fh_di as fh_di,
-    config_32_baseline_fh_tri as fh_tri,
-    config_33_baseline_fh_mono_from_tri as fh_mono_from_tri,
-)
 
-
-def main():
-    # ******************** git setup ********************
+def _clone_returnn() -> tk.Path:
     clone_r_job = tools.CloneGitRepositoryJob(
         url="https://github.com/rwth-i6/returnn.git",
         commit="aadac2637ed6ec00925b9debf0dbd3c0ee20d6a6",
         checkout_folder_name="returnn",
     )
+    return clone_r_job.out_repository
+
+
+def gmm_and_hybrid():
+    from i6_private.users.gunz.experiments.config_2022_07_baselines import (
+        config_10_baseline_gmm_mono as mono,
+        config_11_baseline_gmm_di as di,
+        config_12_baseline_gmm_tri as tri,
+        config_20_baseline_hybrid as hybrid,
+        config_22_baseline_blstm as blyadstm,
+    )
+
+    returnn_root = _clone_returnn()
 
     with tk.block("gmm"):
         mono_sys = mono.run()
@@ -33,12 +33,32 @@ def main():
             gmm_mono=mono_sys,
             gmm_di=di_sys,
             gmm_tri=tri_sys,
-            returnn_root=clone_r_job.out_repository,
+            returnn_root=returnn_root,
         )
-        blyadstm.run(gmm_4gram=tri_sys, returnn_root=clone_r_job.out_repository)
+        blyadstm.run(gmm_4gram=tri_sys, returnn_root=returnn_root)
+
+
+def fh():
+    from i6_private.users.gunz.experiments.config_2022_07_baselines import (
+        config_10_baseline_gmm_mono as mono,
+        config_30_baseline_fh_mono as fh_mono,
+        config_31_baseline_fh_di as fh_di,
+        config_32_baseline_fh_tri as fh_tri,
+        config_33_baseline_fh_mono_from_tri as fh_mono_from_tri,
+    )
+
+    returnn_root = _clone_returnn()
+
+    with tk.block("gmm"):
+        mono_sys = mono.run()
 
     with tk.block("fh"):
-        fh_mono.run(gmm=mono_sys, returnn_root=clone_r_job.out_repository)
-        fh_di.run(returnn_root=clone_r_job.out_repository)
-        fh_tri.run(returnn_root=clone_r_job.out_repository)
-        fh_mono_from_tri.run(returnn_root=clone_r_job.out_repository)
+        fh_mono.run(gmm=mono_sys, returnn_root=returnn_root)
+        fh_di.run(returnn_root=returnn_root)
+        # fh_tri.run(returnn_root=returnn_root)
+        fh_mono_from_tri.run(returnn_root=returnn_root)
+
+
+def main():
+    gmm_and_hybrid()
+    fh()
